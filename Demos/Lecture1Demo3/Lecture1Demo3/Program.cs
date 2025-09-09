463f5a2b84e4ef130b334245a8b7e307a2898c6c
DareenKN <dkinganjatou1@nait.ca>
2025-09-09 14:01:52 -0600

    Lecture 1

---
+++ Demos/Lecture1Demo3/Lecture1Demo3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture1Demo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comparing arrays
+            int[] a = new int[1];
+            a[0] = 17;
+            int[] b;
+
+            b = a;
+
+            Console.WriteLine($"Initial Values :
+
+
+
+
+        }
+    }
+}
