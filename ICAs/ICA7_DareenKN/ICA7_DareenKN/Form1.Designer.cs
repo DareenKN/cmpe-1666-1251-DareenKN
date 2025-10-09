eb66e0dd20b74bec3ff3bf09847614f95ee414fd
DareenKN <dkinganjatou1@nait.ca>
2025-10-08 20:48:54 -0600

    Created a programers block and designed the UI for ICA 7

---
+++ ICAs/ICA7_DareenKN/ICA7_DareenKN/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ICA7_DareenKN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_DragDrop_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_DisplayCount_Btn = new System.Windows.Forms.Button();
+            this.UI_DisplayMin_Btn = new System.Windows.Forms.Button();
+            this.UI_DisplayMax_Btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.UI_NumOfVal_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_MinVal_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_MaxVal_Tbx = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UI_DragDrop_Tbx
+            // 
+            this.UI_DragDrop_Tbx.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.UI_DragDrop_Tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_DragDrop_Tbx.Location = new System.Drawing.Point(35, 120);
+            this.UI_DragDrop_Tbx.Multiline = true;
+            this.UI_DragDrop_Tbx.Name = "UI_DragDrop_Tbx";
+            this.UI_DragDrop_Tbx.ReadOnly = true;
+            this.UI_DragDrop_Tbx.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.UI_DragDrop_Tbx.Size = new System.Drawing.Size(233, 290);
+            this.UI_DragDrop_Tbx.TabIndex = 0;
+            // 
+            // UI_DisplayCount_Btn
+            // 
+            this.UI_DisplayCount_Btn.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.UI_DisplayCount_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_DisplayCount_Btn.Location = new System.Drawing.Point(307, 120);
+            this.UI_DisplayCount_Btn.Name = "UI_DisplayCount_Btn";
+            this.UI_DisplayCount_Btn.Size = new System.Drawing.Size(142, 42);
+            this.UI_DisplayCount_Btn.TabIndex = 1;
+            this.UI_DisplayCount_Btn.Text = "Display Count";
+            this.UI_DisplayCount_Btn.UseVisualStyleBackColor = false;
+            // 
+            // UI_DisplayMin_Btn
+            // 
+            this.UI_DisplayMin_Btn.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.UI_DisplayMin_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_DisplayMin_Btn.Location = new System.Drawing.Point(307, 168);
+            this.UI_DisplayMin_Btn.Name = "UI_DisplayMin_Btn";
+            this.UI_DisplayMin_Btn.Size = new System.Drawing.Size(142, 42);
+            this.UI_DisplayMin_Btn.TabIndex = 2;
+            this.UI_DisplayMin_Btn.Text = "Display Minimum";
+            this.UI_DisplayMin_Btn.UseVisualStyleBackColor = false;
+            // 
+            // UI_DisplayMax_Btn
+            // 
+            this.UI_DisplayMax_Btn.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.UI_DisplayMax_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_DisplayMax_Btn.Location = new System.Drawing.Point(307, 216);
+            this.UI_DisplayMax_Btn.Name = "UI_DisplayMax_Btn";
+            this.UI_DisplayMax_Btn.Size = new System.Drawing.Size(142, 42);
+            this.UI_DisplayMax_Btn.TabIndex = 3;
+            this.UI_DisplayMax_Btn.Text = "Display Maximum";
+            this.UI_DisplayMax_Btn.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(485, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number of Values:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(485, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Minimum Value:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(485, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Maximum Value:";
+            // 
+            // UI_NumOfVal_Tbx
+            // 
+            this.UI_NumOfVal_Tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_NumOfVal_Tbx.Location = new System.Drawing.Point(637, 130);
+            this.UI_NumOfVal_Tbx.Name = "UI_NumOfVal_Tbx";
+            this.UI_NumOfVal_Tbx.Size = new System.Drawing.Size(88, 22);
+            this.UI_NumOfVal_Tbx.TabIndex = 7;
+            // 
+            // UI_MinVal_Tbx
+            // 
+            this.UI_MinVal_Tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_MinVal_Tbx.Location = new System.Drawing.Point(637, 178);
+            this.UI_MinVal_Tbx.Name = "UI_MinVal_Tbx";
+            this.UI_MinVal_Tbx.Size = new System.Drawing.Size(88, 22);
+            this.UI_MinVal_Tbx.TabIndex = 8;
+            // 
+            // UI_MaxVal_Tbx
+            // 
+            this.UI_MaxVal_Tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_MaxVal_Tbx.Location = new System.Drawing.Point(637, 226);
+            this.UI_MaxVal_Tbx.Name = "UI_MaxVal_Tbx";
+            this.UI_MaxVal_Tbx.Size = new System.Drawing.Size(88, 22);
+            this.UI_MaxVal_Tbx.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(89, 46);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(125, 62);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Drag Drop Your File On the Text Box";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.UI_MaxVal_Tbx);
+            this.Controls.Add(this.UI_MinVal_Tbx);
+            this.Controls.Add(this.UI_NumOfVal_Tbx);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UI_DisplayMax_Btn);
+            this.Controls.Add(this.UI_DisplayMin_Btn);
+            this.Controls.Add(this.UI_DisplayCount_Btn);
+            this.Controls.Add(this.UI_DragDrop_Tbx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UI_DragDrop_Tbx;
+        private System.Windows.Forms.Button UI_DisplayCount_Btn;
+        private System.Windows.Forms.Button UI_DisplayMin_Btn;
+        private System.Windows.Forms.Button UI_DisplayMax_Btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox UI_NumOfVal_Tbx;
+        private System.Windows.Forms.TextBox UI_MinVal_Tbx;
+        private System.Windows.Forms.TextBox UI_MaxVal_Tbx;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ICAs/ICA7_DareenKN/ICA7_DareenKN/Form1.cs
@@ -0,0 +1,32 @@
+﻿/* CMPE1600 - Intermediate Programming
+ * Name: Dareen Kinga Njatou
+ * 
+ * Description: CMPE 1666-ICA07- File Processing 
+ *                Read data from a text file. You will use drag 
+ *                and drop to add a file to your application, 
+ *                read the data from the file, extract the data 
+ *                and add to a List. You will then apply a few in 
+ *                built List method 
+ * 
+ * Date: October 8, 2025
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICA7_DareenKN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
