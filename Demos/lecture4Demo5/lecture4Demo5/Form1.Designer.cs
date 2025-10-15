c1a3ccd9868cdcb5af0c23bce897c35faa207464
DareenKN <dkinganjatou1@nait.ca>
2025-10-15 14:44:25 -0600

    Added demos

---
+++ Demos/lecture4Demo5/lecture4Demo5/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace lecture4Demo5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_MainFile_Dlg = new System.Windows.Forms.OpenFileDialog();
+            this.UI_Display_Lbx = new System.Windows.Forms.ListBox();
+            this.UI_LoadData_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UI_MainFile_Dlg
+            // 
+            this.UI_MainFile_Dlg.FileName = "openFileDialog1";
+            // 
+            // UI_Display_Lbx
+            // 
+            this.UI_Display_Lbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_Display_Lbx.FormattingEnabled = true;
+            this.UI_Display_Lbx.ItemHeight = 16;
+            this.UI_Display_Lbx.Location = new System.Drawing.Point(167, 12);
+            this.UI_Display_Lbx.Name = "UI_Display_Lbx";
+            this.UI_Display_Lbx.Size = new System.Drawing.Size(371, 308);
+            this.UI_Display_Lbx.TabIndex = 0;
+            // 
+            // UI_LoadData_btn
+            // 
+            this.UI_LoadData_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_LoadData_btn.Location = new System.Drawing.Point(275, 339);
+            this.UI_LoadData_btn.Name = "UI_LoadData_btn";
+            this.UI_LoadData_btn.Size = new System.Drawing.Size(170, 45);
+            this.UI_LoadData_btn.TabIndex = 1;
+            this.UI_LoadData_btn.Text = "Load Data";
+            this.UI_LoadData_btn.UseVisualStyleBackColor = true;
+            this.UI_LoadData_btn.Click += new System.EventHandler(this.UI_LoadData_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UI_LoadData_btn);
+            this.Controls.Add(this.UI_Display_Lbx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog UI_MainFile_Dlg;
+        private System.Windows.Forms.ListBox UI_Display_Lbx;
+        private System.Windows.Forms.Button UI_LoadData_btn;
+    }
+}
+
---
+++ Demos/week7day2Sorting_Selection_Sort/week7day2Sorting_Selection_Sort/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7day2Sorting_Selection_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array to stores numbers
+            int[] numbers = { 56, 7, 4, 9, 21, 37, 17, 91 };
+
+            // Print array before sorting
+            Console.Write("List before Sorting: ");
+            PrintValues(numbers);
+
+            // Sort the list using selection sort method
+            SelectionSort(numbers);
+
+            // Print array after sorting
+            Console.Write("\nList before Sorting: ");
+            PrintValues(numbers);
+
+            Console.ReadKey();
+        }
+
+        public static void PrintValues(int[] list)
+        {
+            foreach (int item in list)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+
+        /// <summary>
+        /// Method to perform selection sort
+        /// </summary>
+        /// <param name="arr">Array to sort</param>
+        /// <return>Nothimg</return>
+        public static void SelectionSort(int[] arr)
+        {
+            int numberOfElements = arr.Length;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minElementIndex = i;
+
+                // Finding the smallest element's index from rest of the list
+                for (int j = i +1; j<numberOfElements; ++j)
+                {
+                    if (arr[j] < arr[minElementIndex])
+                    {
+                        minElementIndex = j;
+                    }
+                }
+
+                // Swap the found minimum element with the first element
+                int temp = arr[minElementIndex];
+                arr[minElementIndex] = arr[i];
+                arr[i] = temp;
+            }
+
+        }
+    }
+}
