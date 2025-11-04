ec837b2a44bb4d79b8e8141b7143f98b256af123
DareenKN <dkinganjatou1@nait.ca>
2025-11-04 13:25:22 -0700

    Preped for the exam

--- Demos/Exam Prep/File Saving.cs
+++ Demos/Exam Prep/File Saving.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lecture4Demo1
+namespace Exam_Preparation
 {
     public partial class Form1 : Form
     {
@@ -18,38 +18,39 @@ namespace Lecture4Demo1
         public Form1()
         {
             InitializeComponent();
-
-         }
+        }
 
         private void UI_AddToList_Btn_Click(object sender, EventArgs e)
         {
-            //We verify if the textbox is not empty
             if (UI_Input_Tbx.Text.Length > 0)
-            {              
-                //Adds value to textList
+            {
+                // Add values to the textList
                 textList.Add(UI_Input_Tbx.Text);
-                //Adds value to listbox
+
+                // Add values to the list box
                 UI_Display_Lbx.Items.Add(UI_Input_Tbx.Text);
-                //Clears the textbox
+
+                // Clear the textbox
                 UI_Input_Tbx.Clear();
 
-                //if we have added 10 values to the textList
-                //we disable the "Add To List" button and 
-                //enable to "Save to File" button
-                if (textList.Count >= 10)
+                // If we have added 10 values to the textList
+                // We disable the "Add to List" button and 
+                // Enable to "Save to File" button
+                if(textList.Count == 10)
                 {
                     UI_AddToList_Btn.Enabled = false;
                     UI_SaveToFile_Btn.Enabled = true;
                 }
+
             }
         }
 
         private void UI_SaveToFile_Btn_Click(object sender, EventArgs e)
         {
-            //We'll write the list in the file "file1.txt" in the default folder
-            File.WriteAllLines("file1.txt", textList);
+            // We'll write the list in the file "File1.txt" in the default folder
             File.WriteAllLines("../../../../file1.txt", textList);
-            MessageBox.Show("Data has been saved to the file \"file1.txt\" ");
+            MessageBox.Show("Data has been saved to the fle \"File1.txt\""); 
+
         }
     }
 }
